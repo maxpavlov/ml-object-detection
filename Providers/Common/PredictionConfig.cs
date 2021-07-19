fb53af971ac8c23ba3c5dbcf000ce6a9ebd03da3
Max Pavlov <max@radacode.com>
2021-07-19 16:16:41 +0300

    De-dockerized the config, redirected the model file

--- Providers/Common/PredictionConfig.cs
+++ Providers/Common/PredictionConfig.cs
@@ -1,11 +1,12 @@
 using System;
+using System.IO;
 
 namespace waoeml.Providers.Common
 {
     public class PredictionConfig
     {
-        public string ModelProvider => Environment.GetEnvironmentVariable("MODEL_PROVIDER") ?? throw new ArgumentNullException("MODEL_PROVIDER cannot be null");
-        public string ModelFile => Environment.GetEnvironmentVariable("MODEL_FILE") ?? throw new ArgumentNullException("MODEL_FILE cannot be null");
+        public string ModelProvider => Environment.GetEnvironmentVariable("MODEL_PROVIDER") ?? "YOLOV4";
+        public string ModelFile => Environment.GetEnvironmentVariable("MODEL_FILE") ?? Path.GetFullPath("temp/yolov4/yolov4.onnx");
         public int MaxHeight => 480;
     }
 }
